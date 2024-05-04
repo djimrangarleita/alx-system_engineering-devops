3a569f1ab216b31b3ef06eaa9081edf4d237ed77
Djimra NGARLEITA <dngarleita@gmail.com>
2024-05-05 02:51:01 +0300

    fix(#5): replace test file with the right one

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,15 +1,35 @@
-#!/usr/bin/env bash
-# Install nginx web server, edit index.html file and configure redirection
-FILE='/etc/nginx/sites-available/default'
-NEW_URL='https://www.youtube.com/watch?v=QH2-TGUlwu4'
-REDIR_404="\n\terror_page 404 /c_404.html;\n\tlocation = /c_404.html {\n\t\troot /usr/share/nginx/html;\n\t\tinternal;\n\t}"
+# Install new nginx, configure and create 301 redirection
+exec { 'Update system':
+  path        => '/usr/bin',
+  command     => 'apt-get update',
+  refreshonly => true,
+}
 
-apt-get -y update
-apt-get -y install nginx
-mkdir -p /var/www/html
-echo 'Hello World!' > /var/www/html/index.html
-mkdir -p /usr/share/nginx/html
-sed -i "/server_name _;/a\ \trewrite ^/redirect_me ${NEW_URL} permanent;" $FILE
-echo -e "Ceci n'est pas une page\n" > /usr/share/nginx/html/c_404.html
-sed -i "/rewrite .* permanent;/a\ ${REDIR_404}" $FILE
-service nginx restart
+package { 'nginx':
+  ensure => installed
+}
+
+file { '/var/www/html/index.html':
+  ensure  => present,
+  content => "Hello World!\n"
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => present,
+  content => "server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
+	root /var/www/html;
+	index index.html index.htm index.nginx-debian.html;
+	server_name _;
+	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+	location / {
+		try_files \$uri \$uri/ =404;
+	}
+}
+"
+}
+
+exec { 'Restart server':
+  command => '/usr/sbin/service nginx restart'
+}
