e9054a22441bd7331f814cc18aa48ac1e336af51
Djimra NGARLEITA <dngarleita@gmail.com>
2024-05-14 21:46:01 +0300

    fix(#2): remove other configs

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,47 +1,16 @@
-# Install new nginx, configure and create 301 redirection
-exec { 'Update system':
-  path        => '/usr/bin',
-  command     => 'apt-get update',
-  refreshonly => true,
+# Install new nginx and configure it 
+exec {'update':
+  command => '/usr/bin/apt-get update',
 }
-
 -> package { 'nginx':
-  ensure => installed
+  ensure => installed,
 }
-
--> file { '/var/www/html/index.html':
-  ensure  => present,
-  content => "Hello World!\n"
+-> file_line { 'header_served_by':
+  path  => '/etc/nginx/sites-available/default',
+  match => '^server {',
+  line  => "server {\n\tadd_header X-Served-By \"${hostname}\";",
+  multiple => false,
 }
-
--> file { '/usr/share/nginx/html/c_404.html':
-  ensure  => present,
-  content => "Ceci n'est pas une page\n\n"
-}
-
--> file { '/etc/nginx/sites-available/default':
-  ensure  => present,
-  content => "server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
-	root /var/www/html;
-	index index.html index.htm index.nginx-debian.html;
-	server_name _;
-	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
-	error_page 404 /c_404.html;
-	add_header X-Served-By ${hostname};
-	location = /c_404.html {
-		root /usr/share/nginx/html;
-		internal;
-	}
-}
-"
-}
-
--> exec { 'Stop server':
-  command => '/usr/sbin/service nginx stop'
-}
-
--> exec { 'Start server':
-  command => '/usr/sbin/service nginx start'
+-> exec {'run':
+  command => '/usr/sbin/service nginx restart',
 }
