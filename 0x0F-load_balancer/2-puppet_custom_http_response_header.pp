b06a2042c9bcfd9c2b3106a82c42b45ee7c4af16
Djimra NGARLEITA <dngarleita@gmail.com>
2024-05-14 19:24:49 +0300

    chore(#2): add puppet manifest for http header

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,38 @@
+# Install new nginx, configure and create 301 redirection
+exec { 'Update system':
+  path        => '/usr/bin',
+  command     => 'apt-get update',
+  refreshonly => true,
+}
+
+package { 'nginx':
+  ensure => installed
+}
+
+file { '/var/www/html/index.html':
+  ensure  => present,
+  content => "Hello World!\n"
+}
+
+$hostname_output = inline_template('<%= @host_name %>')
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => present,
+  content => "server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
+	root /var/www/html;
+	index index.html index.htm index.nginx-debian.html;
+	server_name _;
+	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+	location / {
+		add_header X-Served-By ${hostname_output};
+		try_files \$uri \$uri/ =404;
+	}
+}
+"
+}
+
+exec { 'Restart server':
+  command => '/usr/sbin/service nginx restart'
+}
