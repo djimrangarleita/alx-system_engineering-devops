ef582c909a31ccb92b3aa4f487a5acd708691b59
Djimra NGARLEITA <dngarleita@gmail.com>
2024-06-04 23:58:46 +0300

    feat(#0): read and print data from api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Read data from API source"""
+
+
+import requests
+from sys import argv
+
+url_todos = f'https://jsonplaceholder.typicode.com/todos?userId={argv[1]}'
+url_users = f'https://jsonplaceholder.typicode.com/users/{argv[1]}'
+
+
+def read_json():
+    """Read and print json data from API"""
+    name = get_user()
+    if (not name):
+        return
+    completed_tasks = 0
+    total = 0
+    todos_list = ''
+    r = requests.get(url_todos)
+    todos = r.json()
+    for task in todos:
+        total += 1
+        if task.get('completed'):
+            completed_tasks += 1
+            todos_list += ('\t' + task.get('title') + '\n')
+    print(f"Employee {name} is done with tasks({completed_tasks}/{total}):")
+    print(todos_list, end='')
+
+
+def get_user():
+    """Print user task"""
+    r = requests.get(url_users)
+    user = r.json()
+    return user.get('name') if user else None
+
+
+if __name__ == "__main__":
+    read_json()
