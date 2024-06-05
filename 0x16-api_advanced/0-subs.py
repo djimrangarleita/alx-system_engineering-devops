467d0aff139735b9c9dc4edfc44803510924008b
Djimra NGARLEITA <dngarleita@gmail.com>
2024-06-05 22:18:52 +0300

    fix(#0): use double quote

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""Query reddit api and return the number of subscribers of a subreddit"""
+"""
+Query reddit api and return the number of subscribers of a subreddit
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
     """Return number of subcribers"""
     endpoint = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': "PersonalAgent/4.0"}
+    headers = {"User-Agent": "PersonalAgent/4.0"}
     response = requests.get(endpoint, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         return 0
-    response = response.json()
-    data = response.get('data')
-    return data.get('subscribers')
+    data = response.json().get("data")
+    result = data.get("subscribers")
+    return result
