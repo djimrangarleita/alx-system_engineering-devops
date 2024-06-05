1b5284499d6f2b62125298507446d9aca820421e
Djimra NGARLEITA <dngarleita@gmail.com>
2024-06-06 02:14:43 +0300

    fix(#0): specify full path to sed in one command

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
 # Replace all occurences of phpp with php in wp-settings.php
 
 exec {'Replace':
-  path    => '/usr/bin',
-  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+  command => '/usr/bin/sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
