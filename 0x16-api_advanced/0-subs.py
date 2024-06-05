d29d4444e516cdb02428eaaf610c4c3f2ac6905b
Djimra NGARLEITA <dngarleita@gmail.com>
2024-06-05 03:40:25 +0300

    fix(#0): define a custom user agent

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,9 @@ import requests
 def number_of_subscribers(subreddit):
     """Return number of subcribers"""
     endpoint = f'https://www.reddit.com/r/{subreddit}/about.json'
-    r = requests.get(endpoint, allow_redirects=False)
+    headers = {'User-Agent': "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0\
+) Gecko/20100101 Firefox/47.0"}
+    r = requests.get(endpoint, headers=headers, allow_redirects=False)
     if r.status_code != 200:
         return 0
     r = r.json()
